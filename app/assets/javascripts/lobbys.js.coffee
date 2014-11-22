add9806009e6a292f7accaec652abbe24a0d4933
herpes <herniaan@gmail.com>
2014-11-22 21:50:50 +0900

    ログアウト機能修正

--- app/assets/javascripts/lobbys.js.coffee
+++ app/assets/javascripts/lobbys.js.coffee
@@ -1,3 +1,31 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+class @LoginsClass
+  constructor: (url, useWebsocket) ->
+    # これがソケットのディスパッチャー
+    @dispatcher = new WebSocketRails(url, useWebsocket)
+    # イベントを監視
+    @bindEvents()
+    console.log(@dispatcher)
+
+  bindEvents: () =>
+    # 送信ボタンが押されたらサーバへメッセージを送信
+    #    $('#send').on 'click', @sendMessage
+    # サーバーからnew_messageを受け取ったらreceiveMessageを実行
+    @dispatcher.bind 'receive_message', @receiveMessage
+
+  sendMessage: (event) =>
+    # サーバ側にsend_messageのイベントを送信
+    # オブジェクトでデータを指定
+    user_name = $('#username').val()
+    msg_body = $('#msgbody').val()
+    #@dispatcher.trigger 'receive_message', { name: user_name }
+    @dispatcher.trigger 'login_user', { name: user_name }
+
+  receiveMessage: (message) =>
+    # 受け取ったデータをappend
+    $('.players').append "<li class=\"user\"> #{message.user_name} </li>"
+    console.log(message)
+
+$ ->
+  window.loginsClass = new LoginsClass($('.player').data('uri'), true)
+  @sendMessage
+  console.log('send')
--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,21 +1,21 @@
 # coding: utf-8
 class LoginsController < ApplicationController
   @@user_id = 0
-  @@users = []
+  Texas::Application.config.users = []
 
   def new
     if session[:user_id]
-      logger.debug @@users
+      logger.debug Texas::Application.config.users
       logger.debug session
 
-      if @@users.empty?       
+      if Texas::Application.config.users.empty?       
         # ユーザをDBに保存していないので、sessionが残ったまま
-        # アプリが再起動したりして@@usersが消えた場合は、
+        # アプリが再起動したりしてTexas::Application.config.usersが消えた場合は、
         # ログアウト処理を行う
         session[:user_id] = nil
         session[:user_name] = nil
       else
-        @login_users = @@users
+        @login_users = Texas::Application.config.users
         logger.debug @login_users
         render 'lobby'
         end
@@ -23,26 +23,31 @@ class LoginsController < ApplicationController
   end
   
   def create
-    name = params[:name]
-    @@user_id = 1 if @@user_id.nil?
-    @@users = [] if @@users.nil?
+    logger.debug "users: #{Texas::Application.config.users}"
     logger.debug session
 
-    if !name.empty? 
+    name = params[:name]
+    if session[:user_name] == name
+      logger.debug "リロード"
+      @login_users = Texas::Application.config.users
+      render 'lobby'
+      return 
+    end
+
+    if !name.empty?
       # Userモデルを作成する。DB使わないならnewのみ
       user = User.new(user_id: @@user_id, name: name)
       @@user_id += 1
-      @@users.push(user)
-      @login_users = @@users
+      Texas::Application.config.users.push(user)
+      @login_users = Texas::Application.config.users
       session[:user_id] = user.user_id
       session[:user_name] = name
 
       # 他のログインしているユーザにユーザ情報を送信
       # broadcast_message(:login_users, {:user_name => name})
 
-      logger.debug session
-      logger.debug @@users
-      logger.debug @login_users
+      logger.debug "users: #{Texas::Application.config.users}"
+      logger.debug "login_users: #{@login_users}"
 
       render 'lobby'
     else
@@ -55,17 +60,23 @@ class LoginsController < ApplicationController
 
   def destroy
     # usersからユーザ名除去
-    @@users.delete(session[:user_name])
-
-    logger.debug "users: #{@@users}"
+    Texas::Application.config.users.each do |user|
+      if user.name == session[:user_name]
+        Texas::Application.config.users.delete(user)
+        break
+      end
+    end
+    
+    logger.debug "users: #{Texas::Application.config.users}"
 
     session[:user_id] = nil
     session[:user_name] = nil
-
 
     render 'new'
   end
   
   def lobby
+    logger.debug "LOBBY"
+    logger.debug Texas::Application.config.users
   end
 end
