65e1ac25beed274591b4f37ed9c26f28121c6e3c
herpes <herniaan@gmail.com>
2014-11-27 23:37:37 +0900

    マージ仕切れていなかった箇所を修正

--- app/controllers/texas_controller.rb
+++ app/controllers/texas_controller.rb
@@ -1,29 +1,19 @@
 # coding: utf-8
 class TexasController < ApplicationController
-<<<<<<< HEAD
-    # 52枚のカード
-    ALL_CARDS = ["01s", "02s", "03s", "04s", "05s", "06s",
-=======
 
-  # 場カード
-  Texas::Application.config.cards = []
-
-  def start
-    logger.debug "ゲーム開始"
-    # ゲームで使うカードをランダムに抽出
-    all_cards = ["01s", "02s", "03s", "04s", "05s", "06s",
->>>>>>> cf47769b210b4296ff2175a439649c37be7d8c55
-      "07s", "08s", "09s", "10s", "11s", "12s", "13s",
-      "01c", "02c", "03c", "04c", "05c", "06c",
-      "07c", "08c", "09c", "10c", "11c", "12c", "13c",
-      "01h", "02h", "03h", "04h", "05h", "06h",
-      "07h", "08h", "09h", "10h", "11h", "12h", "13h",
-      "01d", "02d", "03d", "04d", "05d", "06d",
-      "07d", "08d", "09d", "10d", "11d", "12d", "13d"]
+  # 52枚のカード
+  ALL_CARDS = ["01s", "02s", "03s", "04s", "05s", "06s",
+    "07s", "08s", "09s", "10s", "11s", "12s", "13s",
+    "01c", "02c", "03c", "04c", "05c", "06c",
+    "07c", "08c", "09c", "10c", "11c", "12c", "13c",
+    "01h", "02h", "03h", "04h", "05h", "06h",
+    "07h", "08h", "09h", "10h", "11h", "12h", "13h",
+    "01d", "02d", "03d", "04d", "05d", "06d",
+    "07d", "08d", "09d", "10d", "11d", "12d", "13d"]
 
   def new
     logger.debug "ゲーム開始(#{session[:user_name]})"
-
+    
     @login_users = Texas::Application.config.users
 
     logger.debug session
@@ -52,7 +42,6 @@ class TexasController < ApplicationController
       user.keep_tip = 100
       user.bet_tip = 0
     end
-<<<<<<< HEAD
 
     @table_id = 0            # 場ID
     @table_phase = "preflop" # ラウンド種類
@@ -91,29 +80,6 @@ class Table
   # 場からカードをnum枚引く
   def get_cards(num)
     return @cards.slice!(0..num-1)
-=======
-    Texas::Application.config.cards = cards
-
-    @login_users = Texas::Application.config.users
-    @my_id = session[:user_id]
-    @my_name = session[:user_name]
-    @table_cards = Texas::Application.config.cards  # 場カード
-    @table_omote = 0                                # 表にする場カードの枚数
-    @table_tip = 0                                  # 場チップ
-    @table_turn = 0                                 # 当番ID
-    render 'texas'
-  end
-
-  def started
-    @login_users = Texas::Application.config.users
-    @my_id = session[:user_id]
-    @my_name = session[:user_name]
-    @table_cards = Texas::Application.config.cards  # 場カード
-    @table_omote = 0                                # 表にする場カードの枚数
-    @table_tip = 0                                  # 場チップ
-    @table_turn = 0                                 # 当番ID
-    render 'texas'
->>>>>>> cf47769b210b4296ff2175a439649c37be7d8c55
   end
 
 end
