f233cdcb17fb2d97b69c5ee20b34cf6e5ea5bdc7
iwata <iwata>
2014-11-29 21:31:26 +0900

    test

--- app/controllers/texas_controller.rb
+++ app/controllers/texas_controller.rb
@@ -1,42 +1,6 @@
 # coding: utf-8
 class TexasController < ApplicationController
 
-<<<<<<< HEAD
-  # 場カード
-  Texas::Application.config.cards = []
-  # 当番
-  Texas::Application.config.turn_id = 0
-
-  def start
-    logger.debug "ゲーム開始"
-    # ゲームで使うカードをランダムに抽出
-    all_cards = ["01s", "02s", "03s", "04s", "05s", "06s",
-      "07s", "08s", "09s", "10s", "11s", "12s", "13s",
-      "01c", "02c", "03c", "04c", "05c", "06c",
-      "07c", "08c", "09c", "10c", "11c", "12c", "13c",
-      "01h", "02h", "03h", "04h", "05h", "06h",
-      "07h", "08h", "09h", "10h", "11h", "12h", "13h",
-      "01d", "02d", "03d", "04d", "05d", "06d",
-      "07d", "08d", "09d", "10d", "11d", "12d", "13d"]
-    n = Texas::Application.config.users.size * 2 + 5
-    cards = all_cards.sample(n)
-    # 手札とチップを設定
-    Texas::Application.config.users.each do |user|
-      user.hand = cards.slice!(0,2)
-      user.keep_tip = 100
-      user.bet_tip = 0
-    end
-    Texas::Application.config.cards = cards
-    # 当番をランダムに設定
-    Texas::Application.config.turn_id = rand(0..(Texas::Application.config.users.size - 1))
-    @login_users = Texas::Application.config.users
-    @my_id = session[:user_id]
-    @my_name = session[:user_name]
-    @table_cards = Texas::Application.config.cards  # 場カード
-    @table_omote = 0                                # 表にする場カードの枚数
-    @table_tip = 0                                  # 場チップ
-    @turn_id = Texas::Application.config.users[Texas::Application.config.turn_id].user_id    # 当番ID
-=======
   # 52枚のカード
   ALL_CARDS = ["01s", "02s", "03s", "04s", "05s", "06s",
     "07s", "08s", "09s", "10s", "11s", "12s", "13s",
@@ -71,26 +35,13 @@ class TexasController < ApplicationController
    
     logger.debug @table.omote
 
->>>>>>> bf07f8b83c3f95cd10ba99db3f8f7d5165825a05
     render 'texas'
   end
 end
 
-<<<<<<< HEAD
-  def started
-    @login_users = Texas::Application.config.users
-    @my_id = session[:user_id]
-    @my_name = session[:user_name]
-    @table_cards = Texas::Application.config.cards  # 場カード
-    @table_omote = 0                                # 表にする場カードの枚数
-    @table_tip = 0                                  # 場チップ
-    @turn_id = Texas::Application.config.users[Texas::Application.config.turn_id].user_id    # 当番ID
-    render 'texas'
-=======
 # 場クラス
 class Table
-  attr_accessor :omote, :cards, :phase, :tip, :turn_user
-
+  attr_accessor :omote, :cards, :phase, :tip, :turn_user, :open_cards
   PREFLOP = "preflop"
   
   # コンストラクタ
@@ -102,6 +53,7 @@ class Table
     @tip = 0
     @turn_user = turn_user
     @omote = 2 # TODO: 数字より表になっているカードの配列の方が、後々勝利判定するときに便利そう
+    @open_cards = []
   end
 
   # ユーザ追加
@@ -112,7 +64,7 @@ class Table
   # 場からカードをnum枚引く
   def get_cards(num)
     return @cards.slice!(0..num-1)
->>>>>>> bf07f8b83c3f95cd10ba99db3f8f7d5165825a05
   end
 
 end
+
